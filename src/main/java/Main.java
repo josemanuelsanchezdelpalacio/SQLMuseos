51fb126179835356cb8c9d10555ca5dbf3af5e9a
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-11-17 08:44:13 +0100

    creados metodos para listarObras, modificarMuseo e insertarObra

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import database.EmfSingleton;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import libs.Leer;
 
 import static libs.Leer.pedirCadena;
 
@@ -10,10 +11,27 @@ public class Main {
 
     static EntityManagerFactory emf = EmfSingleton.getInstance().getEmf();
     public static EntityManager em = emf.createEntityManager();
+
     public static void main(String[] args) {
-        Consultas.listarObras(em);
-        Consultas.insertarObra(em);
-        desconectar();
+        boolean salir = false;
+        int opcion;
+        do {
+            System.out.println("0. Salir");
+            System.out.println("1. Ej1: Listar obra");
+            System.out.println("2. Ej1b: Modificar museo");
+            System.out.println("3. Ej2: Insertar nueva obra");
+
+            opcion = Leer.pedirEntero("Introduce una opción: ");
+
+            switch (opcion) {
+                case 0 -> {salir = true;}
+                case 1 -> {Consultas.listarObras(em);}
+                case 2 -> {Consultas.modificarMuseo(em);}
+                case 3 -> {Consultas.insertarObra(em);}
+                default -> {System.out.println("La opcion seleccionada no existe");}
+            }
+            desconectar();
+        } while (!salir);
     }
 
     private static void desconectar() {
--- src/main/java/database/Consultas.java
+++ src/main/java/database/Consultas.java
@@ -6,6 +6,7 @@ import entities.ObrasEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
+import libs.Leer;
 
 import java.sql.Date;
 import java.util.List;
@@ -23,10 +24,12 @@ public class Consultas {
         EntityTransaction transaction = em.getTransaction();
         //comenzamos a crear el contexto de persistencia
         transaction.begin();
-        ObrasEntity obra = em.find(ObrasEntity.class, obraId);
+
         //a partir de aquí trabajamos sobre el objeto instanciado que representa un registro de la base de datos
-        int obraId, nuevoMuseoId;
-        MuseosEntity nuevoMuseo = em.find(MuseosEntity.class, nuevoMuseoId);
+        int idObra = Leer.pedirEntero("Introduce un ID para la obra a modificar: ");
+        int idMuseo = Leer.pedirEntero("Introduce un ID para el museo a modificar: ");
+        ObrasEntity obra = em.find(ObrasEntity.class, idObra);
+        MuseosEntity nuevoMuseo = em.find(MuseosEntity.class, idMuseo);
 
         if (obra != null && nuevoMuseo != null) {
             obra.setMuseosByIdMuseo(nuevoMuseo);
@@ -41,22 +44,18 @@ public class Consultas {
         EntityTransaction transaction = em.getTransaction();
         //comenzamos a crear el contexto de persistencia
         transaction.begin();
-        
-	    String titulo, nombreAutor, nacionalidad;
-	    Date fecha, fechaNacimiento, fechaFallecimiento; 
-	    int idMuseo;
 	
         AutoresEntity autorNuevo = new AutoresEntity();
-        autorNuevo.setNombre(nombreAutor);
-        autorNuevo.setFechaNacimiento(fechaNacimiento);
-        autorNuevo.setFechaFallecimiento(fechaFallecimiento);
-        autorNuevo.setNacionalidad(nacionalidad);
+        autorNuevo.setNombre("nombreAutor");
+        autorNuevo.setFechaNacimiento(Date.valueOf("01-01-1973"));
+        autorNuevo.setFechaFallecimiento(Date.valueOf("01-01-2023"));
+        autorNuevo.setNacionalidad("español");
         em.persist(autorNuevo);
 
         ObrasEntity nuevaObra = new ObrasEntity();
-        nuevaObra.setTitulo(titulo);
-        nuevaObra.setFecha(fecha);
-        nuevaObra.setIdMuseo(idMuseo);
+        nuevaObra.setTitulo("titulo obra");
+        nuevaObra.setFecha(Date.valueOf("01-01-2017"));
+        nuevaObra.setIdMuseo(1);
         nuevaObra.setIdAutor(autorNuevo.getId());
         nuevaObra.setAutoresByIdAutor(autorNuevo);
 
