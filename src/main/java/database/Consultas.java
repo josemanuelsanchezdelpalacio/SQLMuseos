9280cf860e90b7867d650cea9906ba32f4fbf5dd
josemanuelsanchezdelpalacio <92978100+josemanuelsanchezdelpalacio@users.noreply.github.com>
2023-11-17 16:22:54 +0100

    Update Consultas.java

--- src/main/java/database/Consultas.java
+++ src/main/java/database/Consultas.java
@@ -20,92 +20,45 @@ public class Consultas {
         }
     }
 
-    public static void modificarMuseo(EntityManager em){
-        EntityTransaction transaction = em.getTransaction();
-        //comenzamos a crear el contexto de persistencia
-        transaction.begin();
+    public static void modificarMuseo(EntityManager em) {
+    	EntityTransaction transaction = em.getTransaction();
+    	// comenzamos a crear el contexto de persistencia
+    	transaction.begin();
 
-        //a partir de aqui trabajamos sobre el objeto instanciado que representa un registro de la base de datos
-        int idObra = Leer.pedirEntero("Introduce un ID para la obra a modificar: ");
-        int idMuseo = Leer.pedirEntero("Introduce un ID para el museo a modificar: ");
-        ObrasEntity obra = em.find(ObrasEntity.class, idObra);
-        MuseosEntity nuevoMuseo = em.find(MuseosEntity.class, idMuseo);
+    	// a partir de aquí trabajamos sobre el objeto instanciado que representa un registro de la base de datos
+    	int idMuseo = Leer.pedirEntero("Introduce un ID para el museo a modificar: ");
 
-        if (obra != null && nuevoMuseo != null) {
-            obra.setMuseosByIdMuseo(nuevoMuseo);
-            em.persist(obra);
-        }
+    	// Obtener el museo por su ID
+    	MuseosEntity museo = em.find(MuseosEntity.class, idMuseo);
 
-        //al hacer el commit los cambios se pasan a la base de datos
-        transaction.commit();
-    }
+    	if (museo != null) {
+        	// Modificar cada atributo del museo
+        	museo.setNombre(Leer.pedirCadena("Introduce el nuevo nombre del museo: "));
+        	museo.setDireccion(Leer.pedirCadena("Introduce la nueva dirección del museo: "));
+        	museo.setCiudad(Leer.pedirCadena("Introduce la nueva ciudad del museo: "));
+        	museo.setPais(Leer.pedirCadena("Introduce el nuevo país del museo: "));
 
-    public static void modificar(EntityManager em) {
+        	// Utilizar una consulta con parámetros para actualizar cada atributo del museo
+        	Query query = em.createQuery("UPDATE MuseosEntity m SET m.nombre = :nombre, m.direccion = :direccion, m.ciudad = :ciudad, m.pais = :pais WHERE m.id = :idMuseo");
+        	query.setParameter("nombre", museo.getNombre());
+        	query.setParameter("direccion", museo.getDireccion());
+        	query.setParameter("ciudad", museo.getCiudad());
+        	query.setParameter("pais", museo.getPais());
+        	query.setParameter("idMuseo", idMuseo);
 
-        // Pedir al usuario que proporcione el nombre de la tabla
-        String tableName = Leer.pedirCadena("Ingrese el nombre de la tabla que deseas modificar: ");
+        	int updatedEntities = query.executeUpdate();
 
-        // Comprobar que la tabla existe en la base de datos
-        boolean tableExists = em.createNativeQuery("SHOW TABLES LIKE ?").setParameter(1, tableName).getResultList().size() > 0;
-        if (tableExists) {
-            // Mostrar las columnas de la tabla seleccionada
-            List<String> columnNames = em.createNativeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ?").setParameter(1, tableName).getResultList();
-            System.out.println("Columnas de la tabla " + tableName + ":");
+        	if (updatedEntities > 0) {
+            		System.out.println("El museo ha sido modificado exitosamente.");
+        	} else {
+            		System.out.println("No se encontró el museo con el ID proporcionado.");
+        	}
+    	} else {
+        	System.out.println("No se encontró el museo con el ID proporcionado.");
+    	}
 
-            for (String columnName : columnNames) {
-                System.out.println(columnName);
-            }
-
-            // Pedir al usuario que proporcione el nombre de la columna
-            String columnName = Leer.pedirCadena("Ingrese el nombre de la columna que deseas modificar: ");
-
-            // Comprobar que la columna existe en la tabla seleccionada
-            boolean columnExists = em.createNativeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? AND COLUMN_NAME = ?").setParameter(1, tableName).setParameter(2, columnName).getResultList().size() > 0;
-            if (!columnExists) {
-                System.out.println("La columna introducida no existe en la tabla " + tableName + ".");
-                modificar(em);
-            }
-
-            // Pedir al usuario que proporcione el valor de la columna
-            String columnValue = Leer.pedirCadena("Ingrese el valor de la columna: ");
-            modificar(em);
-
-            // Comprobar que el valor de la columna existe en la tabla seleccionada
-            boolean columnValueExists = em.createNativeQuery("SELECT " + columnName + " FROM " + tableName + " WHERE " + columnName + " = ?").setParameter(1, columnValue).getResultList().size() > 0;
-            if (!columnValueExists) {
-                System.out.println("El valor de la columna introducido no existe en la tabla " + tableName + ".");
-                modificar(em);
-            }
-
-            // Pedir al usuario que proporcione el nombre de la columna a modificar
-            String newColumnName = columnName;
-
-            // Si el nombre de la columna es ID_USUARIO, ID_ESTADOS, ID_ENTIDADES, ID_PARTICIPACIONES, ID_PROFESIONES, ID_PROYECTOS, ID_ROLES o ID_TAGS, no se puede modificar
-            if (newColumnName.equals("ID_USUARIO") || newColumnName.equals("ID_ESTADOS") || newColumnName.equals("ID_ENTIDADES") || newColumnName.equals("ID_PARTICIPACIONES") || newColumnName.equals("ID_PROFESIONES") || newColumnName.equals("ID_PROYECTOS") || newColumnName.equals("ID_ROLES") || newColumnName.equals("ID_TAGS")) {
-                System.out.println("No se puede modificar el id ya que es una clave primaria.");
-                return;
-            }
-
-            // Pedir al usuario que proporcione el nuevo valor de la columna
-            String newColumnValue = Leer.pedirCadena("Ingrese el nuevo valor de la columna: ");
-
-            // Crear la sentencia SQL con el nombre de la tabla y los datos a insertar
-            String sql = "UPDATE " + tableName + " SET " + newColumnName + " = '" + newColumnValue + "' WHERE " + columnName + " = '" + columnValue + "'";
-
-            // Crear un objeto Query para ejecutar la sentencia SQL
-            Query query = em.createNativeQuery(sql);
-
-            // Ejecutar la sentencia SQL
-            em.getTransaction().begin();
-            int rowsUpdated = query.executeUpdate();
-            em.getTransaction().commit();
-
-            System.out.println(rowsUpdated + " fila(s) actualizada(s) en la tabla " + tableName);
-
-        } else {
-            System.out.println("La tabla introducida no existe en la base de datos.");
-            modificar(em);
-        }
+    	// al hacer el commit, los cambios se pasan a la base de datos
+    	transaction.commit();
     }
 
     public static void insertarObra(EntityManager em){
